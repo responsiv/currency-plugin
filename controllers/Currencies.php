640c12f449ff0f189009691e6b0df43a1527139e
Pásztor Gábor <gpasztor87@gmail.com>
2016-01-29 14:50:31 +0100

    Add disable form for currencies.

--- controllers/Currencies.php
+++ controllers/Currencies.php
@@ -1,9 +1,13 @@
 <?php namespace Responsiv\Currency\Controllers;
 
+use Lang;
+use Flash;
+use Backend;
 use BackendMenu;
 use Backend\Classes\Controller;
 use System\Classes\SettingsManager;
 use Responsiv\Currency\Models\Currency as CurrencyModel;
+use Exception;
 
 /**
  * Currencies Back-end Controller
@@ -75,5 +79,43 @@ class Currencies extends Controller
         $this->asExtension('FormController')->update_onDelete(post('record_id'));
 
         return $this->listRefresh();
+    }
+
+    public function onLoadDisableForm()
+    {
+        try {
+            $this->vars['checked'] = post('checked');
+        }
+        catch (Exception $ex) {
+            $this->handleError($ex);
+        }
+
+        return $this->makePartial('disable_form');
+    }
+
+    public function onDisableCurrencies()
+    {
+        $enable = post('enable', false);
+        if (($checkedIds = post('checked')) && is_array($checkedIds) && count($checkedIds)) {
+
+            foreach ($checkedIds as $objectId) {
+                if (!$object = CurrencyModel::find($objectId)) {
+                    continue;
+                }
+
+                $object->is_enabled = $enable;
+                $object->save();
+            }
+
+        }
+
+        if ($enable) {
+            Flash::success(Lang::get('responsiv.currency::lang.currency.enable_success'));
+        }
+        else {
+            Flash::success(Lang::get('responsiv.currency::lang.currency.disable_success'));
+        }
+
+        return Backend::redirect('responsiv/currency/currencies');
     }
 }
