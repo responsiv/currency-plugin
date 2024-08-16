73ee1b913e31f5eba8b3eaa4a4e9bc7740e9357d
Samuel Georges <sam@daftspunk.com>
2024-08-16 11:02:48 +1000

    Update breadcrumbs

--- controllers/converters/create.php
+++ controllers/converters/create.php
@@ -1,7 +1,8 @@
 <?php Block::put('breadcrumb') ?>
     <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="<?= Backend::url('responsiv/currency/rates') ?>">Rates</a></li>
-        <li class="breadcrumb-item"><a href="<?= Backend::url('responsiv/currency/converters') ?>">Currency Converters</a></li>
+        <li class="breadcrumb-item"><a href="<?= Backend::url('system/settings') ?>"><?= __("Settings") ?></a></li>
+        <li class="breadcrumb-item"><a href="<?= Backend::url('responsiv/currency/rates') ?>"><?= __("Manage Rates") ?></a></li>
+        <li class="breadcrumb-item"><a href="<?= Backend::url('responsiv/currency/converters') ?>"><?= __("Currency Converters") ?></a></li>
         <li class="breadcrumb-item active" aria-current="page"><?= e($this->pageTitle) ?></li>
     </ol>
 <?php Block::endPut() ?>
--- controllers/rates/preview.php
+++
@@ -1,8 +0,0 @@
-<?php Block::put('breadcrumb') ?>
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="<?= Backend::url('responsiv/currency/rates') ?>">Rates</a></li>
-        <li class="breadcrumb-item active" aria-current="page"><?= e($this->pageTitle) ?></li>
-    </ol>
-<?php Block::endPut() ?>
-
-<?= $this->formRenderDesign() ?>
